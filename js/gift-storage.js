9265b401cbb6bbe72f5f905edb46149aceb2cbaf
starbuckchiang <starbuckchiang@gmail.com>
2026-06-25 13:02:58 +0800

    Update gift-storage.js

--- js/gift-storage.js
+++ js/gift-storage.js
@@ -71,3 +71,5 @@ window.GiftStorage = {
   getRedeemHistory: loadGiftRedeemHistory,
   setRedeemHistory: saveGiftRedeemHistory
 };
+localStorage.setItem('giftPoints', '5000');
+localStorage.setItem('giftTickets', '10');
