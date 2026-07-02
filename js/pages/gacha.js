bd98755210119c697b7591586af664818ff5dff4
starbuckchiang <starbuckchiang@gmail.com>
2026-07-02 18:13:27 +0800

    Update gacha.js

--- js/pages/gacha.js
+++ js/pages/gacha.js
@@ -167,10 +167,4 @@ if (document.readyState === 'loading') {
 } else {
   initGachaPage();
 }
-  await refreshTopbarFromRemote();
-}
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', initGachaPage);
-} else {
-  initGachaPage();
-}
+  
