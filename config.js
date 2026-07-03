7927a1e9682ac25495743a56fe0889fac5f70403
starbuckchiang <starbuckchiang@gmail.com>
2026-07-03 18:41:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
+const SUPABASE_URL = "https://umtqpstacjdwxcvcirbl.supabase.co";
+const SUPABASE_KEY = "sb_publishable_PtWhyYhKGUVxph4o80oGbg_aeZVnUyk";
+
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 
 // =========================
 // App Config / 全站設定值
